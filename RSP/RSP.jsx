2944f2c4028ab903dbdd96e37ebedfd1c1169b83
eunjin0212 <eunjin9639@gmail.com>
2020-11-03 00:05:14 +0900

    RSPChangeHand

--- RSP/RSP.jsx
+++ RSP/RSP.jsx
@@ -6,65 +6,85 @@ const rspCoords = {
   가위: "-142px",
   보: "-284px",
 };
+
 const scores = {
   가위: 1,
   바위: 0,
   보: -1,
 };
+
 const computerChoice = (imgCoord) => {
   return Object.entries(rspCoords).find(function (v) {
     return v[1] === imgCoord;
   })[0];
 };
+
 class RSP extends Component {
   state = {
     result: "",
-    imgCoord: 0,
+    imgCoord: rspCoords.바위,
     score: 0,
   };
 
   interval;
+
   componentDidMount() {
-    // 컴포넌트가 첫 렌더링 후
-    this.interval = setInterval(() => {
-      const { imgCoord } = this.state;
-      if (imgCoord === rspCoords.바위) {
-        this.setState({
-          imgCoord: rspCoords.가위,
-        });
-      } else if (imgCoord === rspCoords.가위) {
-        console.log("hello", this.state.imgCoord, rspCoords.보);
-        this.setState({
-          imgCoord: rspCoords.보,
-        });
-      } else if (imgCoord === rspCoords.보) {
-        this.setState({
-          imgCoord: rspCoords.바위,
-        });
-      }
-    }, 1000);
+    // 컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청을 많이 해요
+    this.interval = setInterval(this.changeHand, 100);
   }
 
-  componentDidUpdate() {
-    // 리렌더링 후
-  }
   componentWillUnmount() {
-    // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 함
+    // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 해요
     clearInterval(this.interval);
   }
-  changeHand = () => {};
-  onClickBtn = (choice) => {
-    // const { imgCoord } = this.state;
-    // const myScore = score[choice];
-    // const cpuScore = score[computerChoice(imgCoord)];
-    // const diff = myScore - cpuScore;
-    // if (diff === 0) {
-    //   this.setState({
-    //     result: "비겼습니다",
-    //   });
-    // } else if ([-1, 2]) {
-    // }
+
+  changeHand = () => {
+    const { imgCoord } = this.state;
+    if (imgCoord === rspCoords.바위) {
+      this.setState({
+        imgCoord: rspCoords.가위,
+      });
+    } else if (imgCoord === rspCoords.가위) {
+      this.setState({
+        imgCoord: rspCoords.보,
+      });
+    } else if (imgCoord === rspCoords.보) {
+      this.setState({
+        imgCoord: rspCoords.바위,
+      });
+    }
   };
+
+  onClickBtn = (choice) => () => {
+    const { imgCoord } = this.state;
+    clearInterval(this.interval);
+    const myScore = scores[choice];
+    const cpuScore = scores[computerChoice(imgCoord)];
+    const diff = myScore - cpuScore;
+    if (diff === 0) {
+      this.setState({
+        result: "비겼습니다!",
+      });
+    } else if ([-1, 2].includes(diff)) {
+      this.setState((prevState) => {
+        return {
+          result: "이겼습니다!",
+          score: prevState.score + 1,
+        };
+      });
+    } else {
+      this.setState((prevState) => {
+        return {
+          result: "졌습니다!",
+          score: prevState.score - 1,
+        };
+      });
+    }
+    setTimeout(() => {
+      this.interval = setInterval(this.changeHand, 100);
+    }, 1000);
+  };
+
   render() {
     const { result, score, imgCoord } = this.state;
     return (
