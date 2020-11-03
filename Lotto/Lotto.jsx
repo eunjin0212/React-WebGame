a07f70e82a7fdb67d46cbb931f01c325e68cf85e
eunjin0212 <eunjin9639@gmail.com>
2020-11-03 17:01:27 +0900

    LottoClassState

--- Lotto/Lotto.jsx
+++ Lotto/Lotto.jsx
@@ -1,1 +1,28 @@
 import React, { Component } from "react";
+import exports from "webpack";
+
+function getWinNumbers() {
+  console.log("getWinNumbers");
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+    );
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
+}
+class Lotto extends Component {
+  state = {
+    winNumbers: getWinNumbers(),
+    winBalls: [],
+    bonus: null,
+    redo: false,
+  };
+  render() {}
+}
+export default Lotto;
