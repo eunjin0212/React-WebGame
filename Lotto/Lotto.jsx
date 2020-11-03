ac444f2bf440568a210f84c67fe271bdfb057a59
eunjin0212 <eunjin9639@gmail.com>
2020-11-03 18:37:12 +0900

    LottoRunTimeOutsFunc

--- Lotto/Lotto.jsx
+++ Lotto/Lotto.jsx
@@ -10,10 +10,13 @@ function getWinNumbers() {
   while (candidate.length > 0) {
     shuffle.push(
       candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+      //splice 기존 요소를 삭제, 교체 또는 새 요소 추가 배열의 내용 변경
     );
   }
   const bonusNumber = shuffle[shuffle.length - 1];
   const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  //sort 정렬 메소드
+  //slice 복사 후 배열 반환
   return [...winNumbers, bonusNumber];
 }
 class Lotto extends Component {
@@ -23,7 +26,52 @@ class Lotto extends Component {
     bonus: null, // 보너스 공
     redo: false,
   };
-  onClickRedo = () => {};
+  timeouts = [];
+
+  runTimeOuts = () => {
+    const { winNumbers } = this.state;
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => {
+          return {
+            winBalls: [...prevState.winBalls, winNumbers[i]],
+          };
+        });
+      }, (i + 1) * 1000);
+    }
+    this.timeouts[6] = setTimeout(() => {
+      this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  };
+
+  componentDidMount() {
+    this.runTimeOuts();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    //조건문이 중요함
+    if (this.winBalls.length === 0) {
+      this.runTimeOuts();
+    }
+  }
+
+  componentWillUnmount() {
+    this.timeouts.forEach((v) => {
+      clearTimeout(v);
+    });
+  }
+  onClickRedo = () => {
+    this.setState({
+      winNumbers: getWinNumbers(), // 당첨 숫자들
+      winBalls: [],
+      bonus: null, // 보너스 공
+      redo: false,
+    });
+    this.timeouts = [];
+  };
   render() {
     const { winBalls, bonus, redo } = this.state;
     return (
@@ -36,7 +84,7 @@ class Lotto extends Component {
         </div>
         <div>보너스</div>
         {bonus && <Ball number={bonus} />}
-        <button onClick={redo ? this.onClickRedo : () => {}}>한 번 더!</button>
+        {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
       </>
     );
   }
