d1a6a96611d53612bf2015e5a598cd1b4d1fc0db
eunjin0212 <eunjin9639@gmail.com>
2020-11-03 17:20:59 +0900

    LottoBallFunctionComponent

--- Lotto/Ball.jsx
+++ Lotto/Ball.jsx
@@ -1,26 +1,27 @@
-import React, { PureComponent } from "react";
-import exports from "webpack";
+import React, { memo } from "react";
 
-class Ball extends PureComponent {
-  render() {
-    const { number } = this.state;
-    let background;
-    if (number <= 10) {
-      background = "red";
-    } else if (number <= 20) {
-      background = "orange";
-    } else if (number <= 30) {
-      background = "yellow";
-    } else if (number <= 40) {
-      background = "blue";
-    } else {
-      background = "green";
-    }
-    return (
-      <div className="ball" style={{ background }}>
-        {number}
-      </div>
-    );
+//함수 컴포넌트
+const Ball = memo(({ number }) => {
+  let background;
+  if (number <= 10) {
+    background = "red";
+  } else if (number <= 20) {
+    background = "orange";
+  } else if (number <= 30) {
+    background = "yellow";
+  } else if (number <= 40) {
+    background = "blue";
+  } else {
+    background = "green";
   }
-}
+  return (
+    <div className="ball" style={{ background }}>
+      {number}
+    </div>
+  );
+});
+
+// class Ball extends PureComponent {
+//   render() {}
+// }
 export default Ball;
