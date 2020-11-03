406418cf976305086ff6c4ca3a47f4aad07b0d39
eunjin0212 <eunjin9639@gmail.com>
2020-11-03 17:17:38 +0900

    LottoBall

--- Lotto/Lotto.jsx
+++ Lotto/Lotto.jsx
@@ -18,11 +18,27 @@ function getWinNumbers() {
 }
 class Lotto extends Component {
   state = {
-    winNumbers: getWinNumbers(),
+    winNumbers: getWinNumbers(), // 당첨 숫자들
     winBalls: [],
-    bonus: null,
+    bonus: null, // 보너스 공
     redo: false,
   };
-  render() {}
+  onClickRedo = () => {};
+  render() {
+    const { winBalls, bonus, redo } = this.state;
+    return (
+      <>
+        <div>당첨 숫자</div>
+        <div id="결과창">
+          {winBalls.map((v) => (
+            <Ball key={v} number={v} />
+          ))}
+        </div>
+        <div>보너스</div>
+        {bonus && <Ball number={bonus} />}
+        <button onClick={redo ? this.onClickRedo : () => {}}>한 번 더!</button>
+      </>
+    );
+  }
 }
 export default Lotto;
