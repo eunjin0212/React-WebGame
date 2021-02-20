f5895bf8d64f327296c07607897c39229b84b4a9
eunjin0212 <eunjin9639@gmail.com>
2021-02-20 13:06:34 +0900

    Convert WordRelay from JS to TSX

---
+++ WordRelay/public/WordRelay.tsx
@@ -0,0 +1,40 @@
+import React, { useState, FormEvent } from "react";
+
+const WordRelay = () => {
+  const [word, setWord] = useState("김은진");
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const inputEl = React.useRef(null);
+
+  const onSubmitForm = (e: FormEvent) => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setResult("딩동댕");
+      setWord(value);
+      setValue("");
+      inputEl.current.focus();
+    } else {
+      setResult("땡");
+      setValue("");
+      inputEl.current.focus();
+    }
+  };
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input
+          id="wordInput"
+          className="wordInput"
+          ref={inputEl}
+          value={value}
+          onChange={(e) => setValue(e.currentTarget.value)}
+        />
+        <button>입력!</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+};
+module.exports = WordRelay;
---
+++ WordRelay/public/WordRelayClass.tsx
@@ -0,0 +1,60 @@
+import React, { Component, FormEvent, ChangeEvent } from 'react'
+
+class WordRelay extends Component {
+  state = {
+    word: "제로초",
+    value: "",
+    result: "",
+  };
+
+  onSubmitForm = (e: FormEvent) => {
+    e.preventDefault();
+    const input = this.input;
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        result: "딩동댕",
+        word: this.state.value,
+        value: "",
+      });
+      if (input) {
+        input.focus();
+      }
+    } else {
+      this.setState({
+        result: "땡",
+        value: "",
+      });
+      if (input) {
+        input.focus();
+      }
+    }
+  };
+  input: HTMLInputElement | null = null // this.input을 생성
+
+  onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ value: e.target.value });
+  };
+
+
+  onRefInput = (c: HTMLInputElement) => {
+    this.input = c;
+  };
+
+  render() {
+    return (
+      <>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            ref={this.onRefInput}
+            value={this.state.value}
+            onChange={this.onChangeInput}
+          />
+          <button>클릭!!!</button>
+        </form>
+        <div>{this.state.result}</div>
+      </>
+    );
+  }
+}
+module.exports = WordRelay;
